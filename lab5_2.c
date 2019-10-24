9a446eb09a94f6527a511fb03254c2b9c0b80db1
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-10-23 21:21:56 -0400

    lab5

---
+++ lab5_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+unsigned long long int num;
+printf("Enter an integer: ");
+scanf("%llu",&num);
+
+if(num == 0){
+	printf("Reverse of %llu is: ",num);
+	printf("%llu",num);
+}
+	
+else {
+		printf("Reverse of %llu is: ",num);
+	}
+
+	
+
+while(num!=0){
+printf("%llu",num%10);
+num=num/10;
+}
+printf("\n");
+return 0;
+}
